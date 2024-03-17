dd2f4ec0eb2f21d7801cd15400837d172733e770
karan-040 <chauhankaran040@gmail.com>
2024-03-17 18:53:40 +0530

    minor bugs fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv"
 import db from "./db/db.connect.js"
 import app from "./app.js"
+import version from "./constants.js"
 dotenv.config({path:"./.env"})
 
 const port = process.env.PORT || 3000
@@ -13,7 +14,7 @@ db.connect()
     })
 
     app.listen(port,()=>{
-        console.log(`server started running on http://localhost:${port}`)
+        console.log(`server started running on http://localhost:${port}${version}/homepage`)
     })
 })
 .catch((error)=>{
